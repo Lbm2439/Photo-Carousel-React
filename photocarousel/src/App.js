ad894813128fcd83c9e297d7612f9d410c758e84
Rapunzel <Laurenminton19@yahoo.com>
2020-03-22 17:05:05 -0400

    corrected photo labels

--- photocarousel/src/App.js
+++ photocarousel/src/App.js
@@ -24,7 +24,7 @@ function App() {
 			</div>
 			<div>
 				<img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" alt="Las Vegas"/>
-				<p className="legend">Las Vegas</p>
+				<p className="legend">New York City</p>
 			</div>
 		</Carousel>
 	</div>
